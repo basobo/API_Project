698197247546f7e1b6bcaded6d572a0ad10ea922
Bruno <brunoasobo@gmail.com>
2020-09-24 13:01:04 -0700

    API program

---
+++ RonWest/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace RonWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.kanyeQuote();
+
+            QuoteGenerator.RonQuote();
+        }
+    }
+}
---
+++ RonWest/QuoteGenerator.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RonWest
+{
+    public class QuoteGenerator
+    {
+        public static void kanyeQuote()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+            var client = new HttpClient();
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"--------------");
+            Console.WriteLine($"kanye:'{kanyeQuote}'");
+            Console.WriteLine($"--------------");
+        }
+        public static void RonQuote ()
+        {
+            var client = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString();
+
+            Console.WriteLine($"--------------");
+            Console.WriteLine($"kanye:'{ronQuote}'");
+            Console.WriteLine($"--------------");
+
+        }
+
+    }
+}
